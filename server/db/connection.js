67921cbfb142260ee118e15fa990515bf75265b0
firstrep <serlaanvesh@gmail.com>
2024-05-05 11:35:00 +0530

    delete .env

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
+ 
  const mongoose = require('mongoose');
 
- const url=`mongodb+srv://chatAppAdmin:Anvesh32@cluster0.ws3j1cp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+ const url=process.env.MONGOURL
  mongoose.connect(url, {
     useNewUrlParser: true, 
     useUnifiedTopology: true
